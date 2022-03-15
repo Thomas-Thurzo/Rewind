4cf1bdf414d8401b3b3682bca73ba774bfbdc1d5
Thomas Thurzo <thomas.thurzo@gmail.com>
2022-03-15 17:29:11 +0100

    Initialisierung

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-// Call Hell World
-function helloWorld(){
-    alert("Hallo Welt");
+
+// Get Text (controller)
+function getValue(){
+
+    let eingabeText = document.getElementById("eingabeString").value;
+    let reverseText = reverseString(eingabeText);
+
+    displayString(reverseText);   
 }
+
+// Reverse Text (logic)
+function reverseString(eingabeText){
+    
+    let reverseTextArray = []; 
+
+    for (let index = eingabeText.length - 1; index >= 0; index--) {
+        reverseTextArray = reverseTextArray + eingabeText[index];       
+    }
+
+    return reverseTextArray;
+}
+
+// Display Text (view)
+function displayString(reverseText){
+
+    document.getElementById("nachricht").innerHTML = "Das umgedrehte Wort lautet: " + reverseText;
+
+    document.getElementById("alert").classList.remove("invisible");
+}
+
